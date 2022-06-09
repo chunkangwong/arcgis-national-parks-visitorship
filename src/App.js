2401536cf1baf08b17fa0710fbec4a6c431b2a31
chunkangwong <chunkang.wong94@gmail.com>
2022-06-09 22:40:33 +0800

    Added ArcGIS Map dependencies

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import {
-  CalciteButton,
-  CalciteIcon,
-  CalciteSlider,
-} from "@esri/calcite-components-react";
-import "@esri/calcite-components/dist/calcite/calcite.css";
-import "@esri/calcite-components/dist/components/calcite-button";
-import "@esri/calcite-components/dist/components/calcite-icon";
-import "@esri/calcite-components/dist/components/calcite-slider";
-import React, { useState } from "react";
+import { Map } from "@esri/react-arcgis";
+import React from "react";
 
 function App() {
-  const [sliderValue, setSliderValue] = useState(50);
-
   return (
-    <>
-      <h1>
-        Hello, React <CalciteIcon icon="banana" />
-      </h1>
-      <CalciteButton onClick={(e) => setSliderValue(0)}>Reset</CalciteButton>
-      <CalciteSlider
-        min="1"
-        max="100"
-        value={sliderValue}
-        step="1"
-        onCalciteSliderUpdate={(e) => setSliderValue(e.target.value)}
-      />
-      <p>The slider currently has a value of {sliderValue}</p>
-    </>
+    <Map
+      mapProperties={{ basemap: "satellite" }}
+      viewProperties={{
+        center: [-70, 25],
+        zoom: 4,
+      }}
+    ></Map>
   );
 }
 
